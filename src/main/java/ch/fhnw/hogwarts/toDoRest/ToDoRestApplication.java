1318f7c9e44f9f6b4153c824cf6b368449a79d0c
Pascal Andermatt <pascal.andermatt@students.fhnw.ch>
2019-06-24 17:41:29 +0200

    initial

---
+++ src/main/java/ch/fhnw/hogwarts/toDoRest/ToDoRestApplication.java
@@ -0,0 +1,13 @@
+package ch.fhnw.hogwarts.toDoRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoRestApplication.class, args);
+	}
+
+}
